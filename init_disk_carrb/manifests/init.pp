5d60da507cc45cf79c99fbbd300f3a24c90904a4
Brian Carr <BRIAN712@bailliegifford.com>
2018-12-07 08:25:18 +0000

    updated modules path

--- init_disk_carrb/manifests/init.pp
+++ init_disk_carrb/manifests/init.pp
@@ -1,11 +1,11 @@
-class int_disk_carrb {
+class init_disk_carrb {
   scriptfile = "new-datadisk.ps1"
   $powershellexe = 'C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe'
   $scriptpath = "C:/Windows/Temp/${wsrpsscriptfile}"
   file { 'new-datadisk.ps1':
     path   => $scriptpath,
     ensure => "file",
-    source => "puppet:///files/$scriptfile",
+    source => "puppet:///modules/init_disk_carrb/files/$scriptfile",
   }
   exec { 'Init Disk and Format':
     command  => "start-process -verb runas $powershellexe -argumentlist '-file ${wsrpsscriptpath}'",
