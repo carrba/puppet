e109ed581cd892b4d5c6192dea48ee5afc920255
bcarr <bcarr@domain.com>
2018-12-13 10:45:29 +0000

    typo in scriptfile path

--- init_disk_carrb/manifests/init.pp
+++ init_disk_carrb/manifests/init.pp
@@ -1,7 +1,7 @@
 class init_disk_carrb {
   $scriptfile = "new-datadisk.ps1"
   $powershellexe = 'C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe'
-  $scriptpath = 'C:/Windows/Temp/${scriptfile}'
+  $scriptpath = 'C:/Windows/Temp/${$scriptfile}'
   file { 'new-datadisk.ps1':
     path   => $scriptpath,
     ensure => "file",
