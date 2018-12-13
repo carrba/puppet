74a05556890f3a7ee972e6826bf83de4a204d1d1
bcarr <bcarr@domain.com>
2018-12-13 10:43:43 +0000

    added some loggin to new-datadisk.ps1

--- init_disk_carrb/files/new-datadisk.ps1
+++ init_disk_carrb/files/new-datadisk.ps1
@@ -1,4 +1,7 @@
-$d = Get-Disk | where PartitionStyle -eq "RAW"
+$logfile = "c:\windows\temp\new-datadisk.log"
+Add-content -path "c:\windows\temp\new-datadisk.log" -value (get-date)
+$d = Get-Disk | where-object PartitionStyle -eq "RAW"
+Add-Content -path $logfile -Value $d.UniqueId
 Initialize-Disk -UniqueId $d.UniqueId # Does as GPT by default, -PartitionStyle GPR/MBT
 
 New-Partition -DriveLetter "S" $d.DiskNumber -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "SysVol" -Confirm:$false
