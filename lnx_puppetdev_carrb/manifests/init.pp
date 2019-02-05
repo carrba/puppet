13537bc9f0ce364cd1191ecd89460d5fea9923c2
bcarr <bcarr@domain.com>
2019-02-05 09:12:17 +0000

    Adding two new Linux modules

---
+++ lnx_puppetdev_carrb/manifests/init.pp
@@ -0,0 +1,28 @@
+class lnx_puppetdev_carrb {
+  package { 'git':
+    ensure   => 'installed',
+  }
+
+  package { 'ruby':
+    ensure   => 'installed',
+  }
+
+  package { 'puppet':
+    ensure   => 'installed',
+    provider => 'gem',
+  }
+  package { 'puppet-lint':
+    ensure   => 'installed',
+    provider => 'gem',
+  }
+
+  package { 'puppet-syntax':
+    ensure   => 'installed',
+    provider => 'gem',
+  }
+
+  package { 'puppetlabs_spec_helper':
+    ensure   => 'installed',
+    provider => 'gem',
+  }
+}
