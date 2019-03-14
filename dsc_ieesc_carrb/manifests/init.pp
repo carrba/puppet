c27cec8faad9d9fc5a119365d206caad309cb357
Brian Carr <BRIAN712@bailliegifford.com>
2019-03-14 16:47:39 +0000

    renamed disable ieesc

--- dsc_ieesc_carrb/manifests/init.pp
+++ dsc_ieesc_carrb/manifests/init.pp
@@ -1,4 +1,4 @@
-class dsc_disable_IEESC_carrb {
+class dsc_ieesc_carrb {
     dsc_registry{ 'Disable_IE_ESC_for_Administrators':
       dsc_ensure    => 'Present',
       dsc_key       => 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}',
