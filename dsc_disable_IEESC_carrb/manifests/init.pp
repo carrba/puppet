a4db76be49303f86a9488ac41d874e94f32dc9e7
Brian Carr <BRIAN712@bailliegifford.com>
2019-03-14 16:08:21 +0000

    switched ie esc settings to dsc

---
+++ dsc_disable_IEESC_carrb/manifests/init.pp
@@ -0,0 +1,17 @@
+class dsc_disable_IEESC_carrb {
+    dsc_registry { 'Disable IE ESC for Administrators':
+      dsc_ensure    => 'Present',
+      dsc_key       => 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}',
+      dsc_valuename => 'IsInstalled',
+      dsc_valuedata => '0',
+      dsc_valuetype => 'dword',
+    }
+
+    dsc_registry { 'Disable IE ESC for Users':
+      dsc_ensure    => 'Present',
+      dsc_key       => 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}',
+      dsc_valuename => 'IsInstalled',
+      dsc_valuedata => '0',
+      dsc_valuetype => 'dword',
+    }
+}
