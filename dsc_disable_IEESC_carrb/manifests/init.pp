8da08176e23810e09aa0842d9c220e8eb8f6a695
Brian Carr <BRIAN712@bailliegifford.com>
2019-03-14 16:44:15 +0000

    typo in dsc_disable_IEESC_carrb

--- dsc_disable_IEESC_carrb/manifests/init.pp
+++ dsc_disable_IEESC_carrb/manifests/init.pp
@@ -1,5 +1,5 @@
 class dsc_disable_IEESC_carrb {
-    dsc_registry {'Disable_IE_ESC_for_Administrators':
+    dsc_registry{ 'Disable_IE_ESC_for_Administrators':
       dsc_ensure    => 'Present',
       dsc_key       => 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}',
       dsc_valuename => 'IsInstalled',
@@ -7,7 +7,7 @@ class dsc_disable_IEESC_carrb {
       dsc_valuetype => 'Dword',
     }
 
-    dsc_registry {'Disable_IE_ESC_for_Users':
+    dsc_registry{ 'Disable_IE_ESC_for_Users':
       dsc_ensure    => 'Present',
       dsc_key       => 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}',
       dsc_valuename => 'IsInstalled',
