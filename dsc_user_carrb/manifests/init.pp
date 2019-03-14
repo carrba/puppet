b8bf43536ed190fcbdd97874475e777a3b3ed8a0
Brian Carr <BRIAN712@bailliegifford.com>
2019-03-14 15:30:03 +0000

    fixing missing class def on dsc_user_carrb

--- dsc_user_carrb/manifests/init.pp
+++ dsc_user_carrb/manifests/init.pp
@@ -1,7 +1,9 @@
-  dsc_user{ 'NewUserHieraPwd':
-    dsc_ensure          => 'Present',
-    dsc_fullname        => 'Stephen Fox',
-    dsc_password        => {'user' => 'foxs', 'password' => Sensitive(lookup('serverbuild'))},
-    dsc_username	=> 'foxs',
-    dsc_disabled	=> 'false',
+  class dsc_user_carrb {
+    dsc_user{ 'NewUserHieraPwd':
+      dsc_ensure          => 'Present',
+      dsc_fullname        => 'Stephen Fox',
+      dsc_password        => {'user' => 'foxs', 'password' => Sensitive(lookup('serverbuild'))},
+      dsc_username	=> 'foxs',
+      dsc_disabled	=> 'false',
+    }
   }
